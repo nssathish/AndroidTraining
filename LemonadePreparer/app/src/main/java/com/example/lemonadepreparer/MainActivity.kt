8ff789c41d7efaf0cbbdcc0d3592765c8de96321
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2024-04-10 00:12:33 +0530

    Code refactor as per kotlin style guidelines

--- LemonadePreparer/app/src/main/java/com/example/lemonadepreparer/MainActivity.kt
+++ LemonadePreparer/app/src/main/java/com/example/lemonadepreparer/MainActivity.kt
@@ -3,145 +3,172 @@ package com.example.lemonadepreparer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lemonadepreparer.ui.theme.LemonadePreparerTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             LemonadePreparerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Lemonade()
+                Lemonade()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Lemonade() {
+    var currentStep by remember {
+        mutableIntStateOf(1)
+    }
+    var squeezeCount by remember {
+        mutableIntStateOf(0)
+    }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Lemonade", fontWeight = FontWeight.Bold)
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentStep) {
+                1 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_select,
+                        drawableResourceId = R.drawable.lemon_tree,
+                        contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                        onImageClick = {
+                            currentStep = 2
+                            squeezeCount = (2..4).random()
+                        }
+                    )
+                }
+
+                2 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_squeeze,
+                        drawableResourceId = R.drawable.lemon_squeeze,
+                        contentDescriptionResourceId = R.string.lemon_content_description,
+                        onImageClick = {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                currentStep = 3
+                            }
+                        }
+                    )
+                }
+
+                3 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_drink,
+                        drawableResourceId = R.drawable.lemon_drink,
+                        contentDescriptionResourceId = R.string.lemonade_content_description,
+                        onImageClick = {
+                            currentStep = 4
+                        }
+                    )
+                }
+
+                4 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_empty_glass,
+                        drawableResourceId = R.drawable.lemon_restart,
+                        contentDescriptionResourceId = R.string.empty_glass_content_description,
+                        onImageClick = {
+                            currentStep = 1
+                        }
+                    )
                 }
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun Lemonade(modifier: Modifier = Modifier.wrapContentSize(Alignment.Center)) {
-    var choice by remember {
-        mutableIntStateOf(1)
-    }
-    var choiceDescription by remember {
-        mutableStateOf(labelOfChoice(1))
-    }
-    var squeezes by remember {
-        mutableIntStateOf(Random.nextInt(2, 4))
-    }
-    Column {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Yellow),
-            contentAlignment = Alignment.TopCenter
-        ) {
-            Text(
-                text = "Lemonade",
-                fontSize = 20.sp,
-                modifier = Modifier.padding(8.dp),
-                color = Color.DarkGray,
-                fontWeight = FontWeight.Bold
-            )
-        }
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+    ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = modifier.fillMaxSize()
         ) {
             Button(
-                onClick = {
-                    choice = when (choice) {
-                        1 -> 2
-                        2 -> {
-                            if (squeezes == 0) {
-                                squeezes = Random.nextInt(2, 4)
-                                3
-                            } else {
-                                squeezes--
-                                2
-                            }
-                        }
-
-                        3 -> 4
-                        else -> 1
-                    }
-                    choiceDescription = labelOfChoice(choice)
-                },
-                shape = RoundedCornerShape(48.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFFE6FFE6)
-                )
+                onClick = onImageClick,
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.button_corner_radius)),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
             ) {
                 Image(
-                    painter = when (choice) {
-                        1 -> painterResource(id = R.drawable.lemon_tree)
-                        2 -> painterResource(id = R.drawable.lemon_squeeze)
-                        3 -> painterResource(id = R.drawable.lemon_drink)
-                        else -> painterResource(id = R.drawable.lemon_restart)
-                    }, contentDescription = when (choice) {
-                        1 -> "Lemon Tree"
-                        2 -> "Lemon squeeze"
-                        3 -> "Lemonade drink"
-                        else -> "Lemonade restart"
-                    }, modifier = Modifier
-                        .background(
-                            color = Color(0xFFE6FFE6),
-                            shape = RoundedCornerShape(8.dp)
-                        )
+                    painter = painterResource(id = drawableResourceId),
+                    contentDescription = stringResource(
+                        id = contentDescriptionResourceId
+                    ),
+                    modifier = modifier
+                        .width(dimensionResource(id = R.dimen.button_image_width))
+                        .height(dimensionResource(id = R.dimen.button_image_height))
+                        .padding(dimensionResource(id = R.dimen.button_interior_padding))
                 )
             }
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_vertical)))
             Text(
-                text = choiceDescription,
-                modifier = modifier.padding(20.dp),
-                fontSize = 12.sp,
-                color = Color.Gray,
-                fontWeight = FontWeight.SemiBold
+                text = stringResource(id = textLabelResourceId),
+                style = MaterialTheme.typography.bodyLarge
             )
         }
-    }
-}
-
-fun labelOfChoice(choice: Int): String {
-    return when (choice) {
-        1 -> "Tap the lemon tree to select a lemon."
-        2 -> "Keep tapping the lemon to squeeze it."
-        3 -> "Tap the lemonade to drink it."
-        else -> "Tap the empty glass to start again."
     }
 }
